0cd6b15653c4fb98c7a368d23f915cec2795d8a3
Zitong Wang <wangzito@shernet.sheridancollege.ca>
2026-03-09 11:08:27 -0400

    Input systeam

---
+++ Assets/Scripts/Controller Input.cs
@@ -0,0 +1,41 @@
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class ControllerInput : MonoBehaviour
+{
+    public float speed = 5;
+    public Vector2 movemnet;
+    public AudioSource SFX;
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //transform.position += (Vector3)movemnet * speed * Time.deltaTime;
+        transform.position = movemnet;
+    }
+
+    public void OnPoint(InputAction.CallbackContext context)
+    {
+        //this is the asme as Mouse.current.position.ReadValue()
+        movemnet = Camera.main.ScreenToWorldPoint(context.ReadValue<Vector2>());
+    }
+
+    public void OnMove(InputAction.CallbackContext context)
+    {
+        context.ReadValue<Vector2>();
+    }
+
+    public void OnAttack(InputAction.CallbackContext context)
+    {
+        Debug.Log("Attack!" + context.phase);
+        if (context.performed == true)
+        {
+            SFX.Play();
+        }
+    }
+}
