9110a5ef79bbe6741701a83c37ffea0428ba583a
Zitong05 <wangzito@sheridancollege.ca>
2026-04-04 15:54:00 -0400

    Player move

--- Assets/System Project/Player Input.cs
+++ Assets/System Project/Player Input.cs
@@ -5,16 +5,32 @@ public class PlayerInput : MonoBehaviour
 {
     public float speed = 5;
     public Vector2 movement;
+    public Animator animator;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+        animator = GetComponent<Animator>();
     }
 
     // Update is called once per frame
     void Update()
     {
         transform.position += (Vector3)movement * speed * Time.deltaTime;
+
+        animator.SetFloat("Speed", Mathf.Abs(movement.x));
+
+        Vector3 scale = transform.localScale;
+
+        if (movement.x > 0)
+        {
+            scale.x = Mathf.Abs(scale.x);
+        }
+        else if (movement.x < 0)
+        {
+            scale.x = -Mathf.Abs(scale.x);
+        }
+
+        transform.localScale = scale;
     }
 
     public void OnMove(InputAction.CallbackContext context)
